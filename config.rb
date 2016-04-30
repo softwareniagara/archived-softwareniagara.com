71e58ffcfeee924707f0b665b1c782dc149434e0
Nickolas Kenyeres <nickolas@helpscout.com>
2016-04-30 17:20:43 -0400

    Use webpack for frontend
    Configure webpack for frontend builds

--- config.rb
+++ config.rb
@@ -1,38 +1,22 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
+# Serve pages with no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+#  External pipeline configuration
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ?
+           "BUILD_PRODUCTION=1 ./node_modules/webpack/bin/webpack.js --bail -p" :
+           "BUILD_DEVELOPMENT=1 ./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+         source: ".tmp/dist",
+         latency: 1
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
+# Development-specific configuration
 configure :development do
+  # Use live reload
   activate :livereload
 end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
---
+++ source/javascripts/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  // Testing that stuff works.
+  // TOOD: Delete this once we write some actual JS code.
+  if (__DEVELOPMENT__) {
+    console.log('Running in development mode!');
+  }
+
+  if (__PRODUCTION__) {
+    console.log('Running in production mode!');
+  }
+});
