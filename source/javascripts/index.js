3a81a73605d2f1362d35c76e2bf42c864fe2c0e2
Nickolas Kenyeres <nickolas@helpscout.com>
2016-10-16 14:33:33 -0400

    DRY up the code

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,6 +1,6 @@
 (function($) {  
   $(document).ready(function() {
-    $('.js-open-beacon').on('click', function(e) {
+    $('[href="mailto:info@softwareniagara.com"]').on('click', function(e) {
       e.preventDefault();
       HS && HS.beacon && HS.beacon.open();
     });
