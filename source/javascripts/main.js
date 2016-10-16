b30b4f4602437a7caa3fc9d10053867ec8ca342c
Nickolas Kenyeres <nickolas@helpscout.com>
2016-10-16 14:42:32 -0400

    Destroy all things in prep for v1

--- source/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-//= require 'vendor/jquery'
-//= require 'vendor/jquery.migrate'
-//= require 'vendor/wow'
-//= require 'components/header'
-//= require 'components/events'
-//= require 'components/section'
-//= require_tree .
